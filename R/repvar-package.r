986431b7fd2cf2a60a4535625f251d5b814b6e2b
Zhian Kamvar <zkamvar@gmail.com>
2018-03-07 16:17:48 -0600

    update documentation

--- R/repvar-package.r
+++ R/repvar-package.r
@@ -3,7 +3,13 @@
 #' This package allows you to find the minimum set of samples that represents
 #' all non-zero variables in a binary matrix.
 #'
-#' The underlying function is [get_minimum_set()].
+#' Minimum set of samples can be found with the function [find_samples()]. This
+#' function will shuffle your data set and pass it to [get_minimum_set()]. From
+#' this, you will have a list of sample names.
+#'
+#' Because there may be several combinations of samples that represent all
+#' variables, the function [entropy()] can be used to calculate entropy
+#' statistics over these variables.
 #'
 #' @name repvar
 #' @docType package
